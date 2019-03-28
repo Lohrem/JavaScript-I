201d3295f391c9ab1514064cacfd3b66e4b7f0c9
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-03-27 20:10:24 -0400

    Objects.js is complete

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -68,13 +68,18 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0;
-
-console.log();
+// for (let i = 0; i < inventory.length; i++) {
+//   lastCar = inventory[i];
+// }
+//console.log(`The last car is a ${lastCar.car_make} ${lastCar.car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-console.log();
+for (let i = 0; i < inventory.length; i++) {
+  carModels.push(inventory.car_model);
+}
+console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
